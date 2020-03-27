2be32bc35604776b7be8fd813bd9882fd2de13f5
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-26 19:14:32 -0600

    removed bloatware and added server.js and connection to MongoDB

---
+++ README.md
@@ -0,0 +1,7 @@
+# GlobalChurchHackathon - Back End
+
+## How to Use
+1. Clone to your local machine
+2. Replace "YourUserName" and "YourPassword" with your MongoDB credentials  to .env 
+3. `npm install` in your terminal to install node-modules
+4. `npm start` to open development in your browser
--- app.js
+++ app.js
@@ -1,41 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose')
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  return res.send('Hello World!');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+dotenv.config();
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, {
+  promiseLibrary: require('bluebird'),
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+.then(() => console.log('connection successful'))
+.catch((err) => console.error(err));
+mongoose.Promise = global.Promise;
 
 module.exports = app;
