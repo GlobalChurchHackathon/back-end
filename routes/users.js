0ca1d400e2bf3a9d3d24a6cbb3a782af4339fa80
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-28 09:20:45 -0600

    Create get and post routes for users

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const User = require('../models/users');
+const router = express.Router();
+
+//Get Route - Get All Users
+router.get('/', async (req, res) => {
+    const data = await User.find().sort('name');
+    res.send(data);
+  });
+
+// Post Route - Add New User
+router.post('/', async (req, res) => {
+    // Hash Passwords
+    let salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const data = new User({
+        _id: mongoose.Types.ObjectId(),
+        userName: req.body.userName,
+        password: hashedPassword
+    })
+
+    const savedUser = await data.save();
+    res.send(savedUser);
+});
+
+// Put & Patch Routes - Update User by ID
+
+// Delete Routes - Delete User by ID
+
+module.exports = router;
