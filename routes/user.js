8d7904464f8c31c361f8fadf5674f4eee2130458
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-29 12:27:03 -0600

    added login route

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt')
+const auth = require('../../middleware/auth');
+const bcyrpt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require("../models/User");
 
@@ -25,6 +29,87 @@ router.post('/', async (req, res) => {
   res.send(savedUser);
 });
 
+
+//finds user
+
+//@route    GET api/auth
+//@desc     Test route
+//@access   public
+router.get('/login', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+/*
+@route    POST api/auth
+@desc     Authenticate user & get token
+@access   public */
+
+router.post(
+    "/",
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    //this is the responds
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            //does the user exists?
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid email or password' }] });
+            }
+
+            /*  does the email & password match the user.id? 
+                comparing plain text to encrypted 
+            */
+            const isMatch = await bcyrpt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid email or password' }] });
+            }
+
+            //return jsw
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 3600 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
+
+module.exports = router;
 // PUT route
 router.put("/:id", async (req, res) => {});
 
