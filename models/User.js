744d4cea92afa96855185aa5b7bb6390ea142ac9
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-29 08:25:39 -0600

    adjusted models and user post route to match front end

--- models/User.js
+++ models/User.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
     email: {
         type: String,
         unique: true,
         required: true
     },
     password: {
+        type: String,
+        required: true
+    },
+    password2: {
         type: String,
         required: true
     },
--- routes/user.js
+++ routes/user.js
@@ -15,10 +15,14 @@ router.post('/', async (req, res) => {
   // Hash Passwords
   let salt = await bcrypt.genSalt(10);
   let hashedPassword = await bcrypt.hash(req.body.password, salt);
+  let hashedPassword2 = await bcrypt.hash(req.body.password2, salt);
 
   const data = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
-      password: hashedPassword
+      password: hashedPassword,
+      password2: hashedPassword2
   })
 
   const savedUser = await data.save();
