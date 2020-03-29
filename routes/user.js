ed3e7c9fab4db248ab34c556e79feced85684f08
Judyhouser80 <judyhouser80@gmail.com>
2020-03-29 10:57:07 -0400

    add user login route

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,46 @@ router.get("/", async (req, res) => {
   res.send(users);
 });
 
+// login route - compare passwords and return JWT 
+// token with _id and admin fields - secured
+router.route('/user/login').post((req, res) => {
+    
+    //unsure if this is specifically for angular or if react can use it too.
+  let header = req.headers['authorization'];
+  const bearer = header.split(' ');
+  const token = bearer[1];
+  if (token === 'undefined') {
+    var checkEmail = req.body.email;
+    var checkPassword = req.body.password;
+
+    User.findOne({ email: checkEmail }, (err, user) => {
+      if (!user) {
+        return res.status(401).send('Login Failed, User not found');
+      } if (user) {
+        let passwordMatch = shService.comparePasswords(checkPassword, user.password);
+        if (passwordMatch) {
+          let token = authService.signUser(user); // created token
+          // res.cookie('jwt', token);
+          res.json({
+            userId: user._id,
+            admin: user.admin,
+            token: token
+          });
+          console.log('Successful login! Welcome Friend!!');
+        } else {
+          console.log('Incorrect Password');
+          res.send('Incorrect Password');
+        }
+      }
+    });
+  } else {
+    res.status(401);
+    res.send('User is already logged in');
+  }
+});
+
+
+
 //POST route
 router.post('/', async (req, res) => {
   // Hash Passwords
