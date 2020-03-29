6f5b6ac466219bbe295eacdf73011815dbf6ebf6
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-29 00:49:49 -0600

    added express middleware for axios requests

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors')
 const dotenv = require('dotenv');
 const mongoose = require('mongoose')
 const app = express();
 const bodyParser = require("body-parser");
 
 // Middleware 
+app.use(cors());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 const userRoutes = require("../back-end/routes/user");
 const profileRoutes = require("../back-end/routes/profile");
