ab3b9d1365b82dc2cc29c8abf99fdbafbc80b44e
Elizabeth Munsinger <emunsinger@gmail.com>
2020-03-30 18:32:23 -0500

    add user delete and updated router.put

--- models/Profile.js
+++ models/Profile.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const ProfileSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
     firstName: {
         type: String,
         required: true
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const bcyrpt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { check, validationResult } = require('express-validator');
+// const auth = require('../../middleware/auth');
+const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const config = require('config');
+// const { check, validationResult } = require('express-validator');
 
 
 const User = require("../models/User");
-
+const Profile = require("../models/Profile");
 // GET route
 router.get("/", async (req, res) => {
   const users = await User.find().sort("email");
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
   })
 
 // PUT route
-router.put("update/:id", async (req, res) => {
+router.put("/update/:id", (req, res) => {
     User.findById(req.params.id)
     .then(users => {
         users.firstName = req.body.firstName;
@@ -54,85 +54,89 @@ router.put("update/:id", async (req, res) => {
 //@route    GET api/auth
 //@desc     Test route
 //@access   public
-router.get('/login', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+// router.get('/login', auth, async (req, res) => {
+//     try {
+//         const user = await User.findById(req.user.id).select('-password');
+//         res.json(user);
+//     } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error');
+//     }
+// });
 
 /*
 @route    POST api/auth
 @desc     Authenticate user & get token
 @access   public */
 
-router.post(
-    "/",
-    [
-        check('email', 'Please include a valid email').isEmail(),
-        check('password', 'Password is required').exists()
-    ],
-    //this is the responds
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+// router.post(
+//     "/",
+//     [
+//         check('email', 'Please include a valid email').isEmail(),
+//         check('password', 'Password is required').exists()
+//     ],
+//     //this is the responds
+//     async (req, res) => {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         }
 
-        const { email, password } = req.body;
+//         const { email, password } = req.body;
 
-        try {
-            //does the user exists?
-            let user = await User.findOne({ email });
+//         try {
+//             //does the user exists?
+//             let user = await User.findOne({ email });
 
-            if (!user) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'Invalid email or password' }] });
-            }
+//             if (!user) {
+//                 return res
+//                     .status(400)
+//                     .json({ errors: [{ msg: 'Invalid email or password' }] });
+//             }
 
             /*  does the email & password match the user.id? 
                 comparing plain text to encrypted 
             */
-            const isMatch = await bcyrpt.compare(password, user.password);
+            // const isMatch = await bcyrpt.compare(password, user.password);
 
-            if (!isMatch) {
-                return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'Invalid email or password' }] });
-            }
+            // if (!isMatch) {
+            //     return res
+            //         .status(400)
+            //         .json({ errors: [{ msg: 'Invalid email or password' }] });
+            // }
 
             //return jsw
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
+//             const payload = {
+//                 user: {
+//                     id: user.id
+//                 }
+//             }
 
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'),
-                { expiresIn: 3600 },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                }
-            );
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }
-    }
-);
+//             jwt.sign(
+//                 payload,
+//                 config.get('jwtSecret'),
+//                 { expiresIn: 3600 },
+//                 (err, token) => {
+//                     if (err) throw err;
+//                     res.json({ token });
+//                 }
+//             );
+//         } catch (err) {
+//             console.error(err.message);
+//             res.status(500).send('Server error');
+//         }
+//     }
+// );
 
-module.exports = router;
+// module.exports = router;
 // PUT route
-router.put("/:id", async (req, res) => {});
+// router.put("/:id", async (req, res) => {});
 
 // DELETE route
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  User.findByIdAndDelete(req.params.id)
+  .then(() =>res.json('User deleted.'))
+  .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
