60639e5cf80399934fe6711c907bb26935c6c65f
Ryan Miller <rpmiller@rocketmail.com>
2020-04-01 18:13:35 -0400

    adding geoschema to request

--- models/Request.js
+++ models/Request.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
+// geolocation schema for lng, lat
+const GeoSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates:{
+        type: [Number],
+        index: "2dsphere"
+    }
+});
+
 const RequestSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -12,9 +24,26 @@ const RequestSchema = new mongoose.Schema({
     isClaimed: {
         type: Boolean,
     },
-    location: {
-        type: Array,
+    address1: {
+        type: String,
+        required: true,
+    },
+    address2: {
+        type: String,
+        required: false
+    },
+    city: {
+        type: String,
         required: true
-    }
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    zipCode: {
+        type: Number,
+        required: true
+    },
+    location: GeoSchema
 })
 module.exports = User = mongoose.model('request', RequestSchema);
