8a79e03439d27dfa57ac7641b20cb9b70ba990a0
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-04-02 12:46:46 -0700

    coded delete route for profiles

--- routes/profile.js
+++ routes/profile.js
@@ -15,25 +15,9 @@ router.get("/", async (req, res) => {
     res.send(users);
   });
   
-// router.get('/me', async (req, res) => {
-//     try {
-//       const profile = await Profile.findOne({
-//         user: req.body.id
-//       });
-  
-//       if (!profile) {
-//         return res.status(400).json({ msg: 'There is no profile for this user' });
-//       }
-  
-//       // only populate from user document if profile exists
-//       res.json(profile.populate('user', ['name', 'avatar']));
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).send('Server Error');
-//     }
-//   });
 
-  // @route    GET api/profile/me
+
+// @route    GET api/profile/me
 // @desc     Get current users profile
 // @access   Private
 router.get('/me', async (req, res) => {
@@ -93,45 +77,6 @@ router.post("/",  async (req, res) => {
   });
 
 
-// POST route
-// router.post("/", auth, async (req, res) => {
-//     const {error} = validationResult(req.body);
-
-//     if(error){
-//         return res.status(400).send(error.details[0].message)
-//     }
-
-//     try {
-//         let profile = await User.findOne({userId: req.body.userId})
-
-//         if(!profile) {
-//             return res 
-//             .status(400)
-//             .json({errors: [{ msg: 'Invalid Credentials'}]})
-//         }
-
-//         profile = new Profile ({
-//             firstName: req.body.firstName,
-//             lastName: req.body.lastName,
-//             address1: req.body.address1,
-//             address2: req.body.address2,
-//             city: req.body.city,
-//             state: req.body.state,
-//             zipCode: req.body.zipCode,
-//             phoneNumber:req.body.phoneNumber
-//         })
-
-//         if(profile.userId == req.body.userId) {
-//             await profile.save();
-//             res.send(profile)
-//         }
-//     } catch(err) {
-//         console.error(err.message);
-//         res.status(500).send('Server Error')
-//     }
-
-// })
-
 // PUT route
 router.put("/:id", async (req, res) => {
 
@@ -139,9 +84,35 @@ router.put("/:id", async (req, res) => {
 
 
 // DELETE route
+// router.delete("/:id", async (req, res) => {
+//   User.findByIdAndDelete(req.params.id)
+//   .then(() =>res.json('Profile deleted.'))
+//   .catch(err => res.status(400).json('Error: ' + err));
+// });
 
-router.delete("/:id", async (req, res) => {
+// router.delete('/:user', async (req, res) => {
+//   try {
 
-})
+//     // // Remove profile
+//     await Profile.findOneAndRemove({ user: req.body.user });
+//     res.json({ msg: 'User deleted' });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
+
+
+
+router.delete('/:user', async (req, res) => {
+  const profile = await Profile.findOneAndRemove(req.params.user)
+  console.log(profile);
+  if (!profile) {
+      return res.status(404).json({msg: 'Profile not found'})
+  }
+  res.json({
+      msg: 'Profile Removed'
+  });
+} )
 
 module.exports = router;
