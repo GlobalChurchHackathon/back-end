1be001b381810549ecd0e8b6764d073d84476861
Elizabeth Munsinger <emunsinger@gmail.com>
2020-04-02 16:45:57 -0500

    update profile route.put and Scott's requst.put

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const {  validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Profile = require('../models/Profile');
 
@@ -11,20 +11,20 @@ const Profile = require('../models/Profile');
 //     res.send(profile);
 // })
 router.get("/", async (req, res) => {
-    const users = await Profile.find();
-    res.send(users);
-  });
-  
+  const users = await Profile.find();
+  res.send(users);
+});
+
 // router.get('/me', async (req, res) => {
 //     try {
 //       const profile = await Profile.findOne({
 //         user: req.body.id
 //       });
-  
+
 //       if (!profile) {
 //         return res.status(400).json({ msg: 'There is no profile for this user' });
 //       }
-  
+
 //       // only populate from user document if profile exists
 //       res.json(profile.populate('user', ['name', 'avatar']));
 //     } catch (err) {
@@ -33,7 +33,7 @@ router.get("/", async (req, res) => {
 //     }
 //   });
 
-  // @route    GET api/profile/me
+// @route    GET api/profile/me
 // @desc     Get current users profile
 // @access   Private
 router.get('/me', async (req, res) => {
@@ -56,41 +56,41 @@ router.get('/me', async (req, res) => {
 });
 
 
-router.post("/",  async (req, res) => {
-    const {error} = validationResult(req.body);
+router.post("/", async (req, res) => {
+  const { error } = validationResult(req.body);
 
-    if(error){
-        //return res.status(400).send(error.details[0].message)
-      return res.status(400).json({ msg: 'There is no profile for this user' });
-    }
-  
-    try {
-        console.log("test1")
+  if (error) {
+    //return res.status(400).send(error.details[0].message)
+    return res.status(400).json({ msg: 'There is no profile for this user' });
+  }
 
-        const profile = new Profile ({
-            user: req.body.user,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            address1: req.body.address1,
-            address2: req.body.address2,
-            city: req.body.city,
-            state: req.body.state,
-            zipCode: req.body.zipCode,
-            phoneNumber: req.body.phoneNumber
-        })
-        await profile.save();
-        res.send(profile)
+  try {
+    console.log("test1")
 
-        // if(profile.user == req.body.user.id) {
-        //     await profile.save();
-        //     res.send(profile)
-        // }
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server Error')
+    const profile = new Profile({
+      user: req.body.user,
+      // firstName: req.body.firstName,
+      // lastName: req.body.lastName,
+      address1: req.body.address1,
+      address2: req.body.address2,
+      city: req.body.city,
+      state: req.body.state,
+      zipCode: req.body.zipCode,
+      phoneNumber: req.body.phoneNumber
+    })
+    await profile.save();
+    res.send(profile)
 
-    }
-  });
+    // if(profile.user == req.body.user.id) {
+    //     await profile.save();
+    //     res.send(profile)
+    // }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+
+  }
+});
 
 
 // POST route
@@ -132,10 +132,45 @@ router.post("/",  async (req, res) => {
 
 // })
 
-// PUT route
-router.put("/:id", async (req, res) => {
 
-})
+// PUT route
+// router.put("/:user", (req, res) => {
+//   Profile.findById(req.params.id)
+//     .then(profile => {
+//       // profile.firstName = req.body.firstName;
+//       // profile.lastName = req.body.lastName;
+//       // profile.address1 = req.body.address1;
+//       profile.address2 = req.body.address2;
+//       profile.city = req.body.city;
+//       profile.state = req.body.state;
+//       profile.zipCode = req.body.zipCode;
+//       profile.phoneNumber = req.body.phoneNumber;
+
+//       profile.save()
+//         .then(() => res.json('Profile updated!'))
+//         .catch(err => res.status(400).json('Error: ' + err));
+//     })
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+router.put('/:user',  async (req, res) => {
+  const { error } = validationResult(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+  const profile = await Profile.findOneAndUpdate(
+      req.body.user,
+      {
+          address1: req.body.address1,
+          address2: req.body.address2,
+          city: req.body.city,
+          state: req.body.state,
+          zipCode: req.body.zipCode,
+          phoneNumber: req.body.phoneNumber,
+
+      },
+  );
+  if (!profile) return res.status(404).send("Invalid Credentials")
+      await profile.save();
+      res.send(profile);
+  });
 
 
 // DELETE route
