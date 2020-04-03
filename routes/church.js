253557ae2de89c85adfc6e445d41dcf28b832783
jkinsinger <52002561+jkinsinger@users.noreply.github.com>
2020-04-02 17:40:26 -0700

    got delete route for churches coded and working

--- routes/church.js
+++ routes/church.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const {  validationResult } = require('express-validator');
 
 const Church = require("../models/Church");
 
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
     res.send(churches);
   });
 
-// Post route - Add New User
+// Post route - Add New Church/Organizations
 router.post('/', async (req, res) => {
   
     let churches = await Church.findOne({ email: req.body.email })
@@ -35,6 +35,40 @@ router.post('/', async (req, res) => {
   
     const savedChurch = await church.save();
     res.send(savedChurch);
+  });
+
+  // PUT route
+  router.put('/:id',  async (req, res) => {
+    const { error } = validationResult(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+    const churches = await Church.findByIdAndUpdate(
+        req.body.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            address1: req.body.address1,
+            address2: req.body.address2,
+            city: req.body.city,
+            state: req.body.state,
+            zipCode: req.body.zipCode,
+            phoneNumber: req.body.phoneNumber,
+            socialMedia1: req.body.socialMedia1,
+            socialMedia2: req.body.socialMedia2
+        },
+    );
+    if (!churches) return res.status(404).send("Invalid Credentials")
+        await churches.save();
+        res.send(churches);
+    });
+  
+
+  // DELETE route
+
+  router.delete("/:id", async (req, res) => {
+    Church.findByIdAndDelete(req.params.id)
+    .then(() =>res.json('User deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
   });
 
   module.exports = router;
