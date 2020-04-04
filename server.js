fc299004d58adbcadce4d5cdad5e32b988c6c365
Matthew Gunderson <matthew.gunderson12@gmail.com>
2020-04-04 13:31:32 -0700

    app in one folder

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 4000;
-const server = http.createServer(app);
+const express = require('express');
+const connectDB = require('./config/db');
+// const http = require('http');
+// const app = require('./app');
+const port = process.env.PORT || 5000;
+// const server = http.createServer(app);
+
+//heroku deployment
 const path = require('path')
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('front-end'))
 }
 
-const PORT = process.env.PORT || 4000;
+//init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//define routes
+app.use('/auth', require('./routes/auth'))
+app.use('/church', require('./routes/church'))
+app.use('/profile', require('./routes/profile'))
+app.use('/request', require('./routes/request'))
+app.use('/user', require('./routes/user'))
+
+
+const PORT = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
