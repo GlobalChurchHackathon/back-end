e332b286e8096e67616b1e8e324e4ec3cc457aa4
Matthew Gunderson <matthew.gunderson12@gmail.com>
2020-04-04 13:06:46 -0700

    serverBuild

--- server.js
+++ server.js
@@ -2,4 +2,12 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 4000;
 const server = http.createServer(app);
+const path = require('path')
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('front-end'))
+}
+
+const PORT = process.env.PORT || 4000;
+
 server.listen(port, () => console.log(`Server started on port ${port}`));
